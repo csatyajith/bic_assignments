4f4c906a013853f9e7acb17189a59d93fad1fda7
ashshetty-prog <ashmitha.shetty71@gmail.com>
2020-10-28 14:08:50 -0400

    Plotting spike frequency vs input current

--- HH_model.py
+++ HH_model.py
@@ -7,14 +7,34 @@ class Hodgkin_Huxley:
     def __init__(self, current):
         self.I = current
         self.totaltime = 50
-        self.deltaT = 0.001
+        self.deltaT = 0.01
         self.times = np.arange(0, self.totaltime, self.deltaT)
         self.V, self.INa, self.IK, self.Il, self.NStates, self.MStates, self.HStates = (np.zeros(len(self.times)) for i
                                                                                         in range(7))
-        self.ENa, self.EK, self.El = 115, -12, 10.6
-        self.gNa, self.gK, self.gl = 120, 36, 0.3
-        self.m, self.n, self.h = (g.Gates() for i in range(3))
-        self.C = 1
+        self.ENa, self.EK, self.El = 115, -12, 10.6  # Equilibrium potentials of Na, K and Cl
+        self.gNa, self.gK, self.gl = 120, 36, 0.3  # Conductances of Na, K, Cl
+        self.m, self.n, self.h = (g.Gates() for i in range(3))  # Gating variables
+        self.C = 1  # Membrane Capacitance
+        self.V_thresh = 55
+
+    def plotNeuronSimulation(self):
+        self.V[0] = -55
+        numSpikes = 0
+        for i in range(1, len(self.times)):
+            self.UpdateGateTimeConstants(self.V[i - 1])
+            self.UpdateActionPotential(i)
+            self.updateGateVariables(i)
+            if self.V[i] >= self.V_thresh:
+                numSpikes += 1
+        fig, axs = plt.subplots(2)
+        fig.suptitle("Membrane Voltage of a Neuron over Time with Input Current =" + str(self.I), y=1)
+        axs[0].plot(self.times, self.V)
+        axs[1].plot(self.times, self.MStates, 'tab:orange', label='m')
+        axs[1].plot(self.times, self.NStates, 'tab:green', label='n')
+        axs[1].plot(self.times, self.HStates, 'tab:red', label='h')
+        plt.legend(loc=1)
+        plt.show()
+        return numSpikes
 
     def UpdateGateTimeConstants(self, V):
         self.m.alpha = .1 * ((25 - V) / (np.exp((25 - V) / 10) - 1))
@@ -35,18 +55,16 @@ class Hodgkin_Huxley:
         self.NStates[i] = self.n.updateGateState(self.deltaT)
         self.HStates[i] = self.h.updateGateState(self.deltaT)
 
-hh = Hodgkin_Huxley(10)
-hh.V[0] = -55
-for i in range(1, len(hh.times)):
-    hh.UpdateGateTimeConstants(hh.V[i - 1])
-    hh.UpdateActionPotential(i)
-    hh.updateGateVariables(i)
 
-fig, axs = plt.subplots(2)
-fig.suptitle("Membrane Voltage of a Neuron over Time with Input Current =" + str(hh.I), y=1)
-axs[0].plot(hh.times, hh.V)
-axs[1].plot(hh.times, hh.MStates, 'tab:orange', label='m')
-axs[1].plot(hh.times, hh.NStates, 'tab:green', label='n')
-axs[1].plot(hh.times, hh.HStates, 'tab:red', label='h')
-plt.legend(loc=1)
+input_Current = []
+total_spikes = []
+for cur in range(12):
+    hh = Hodgkin_Huxley(cur)
+    n_spikes = hh.plotNeuronSimulation()
+    input_Current.append(cur)
+    total_spikes.append(n_spikes / (1000 * hh.totaltime))
+plt.plot(input_Current, total_spikes)
+plt.xlabel("Input Current")
+plt.ylabel("Number of Spikes")
+plt.title("Frequency of n_spikes over different input currents")
 plt.show()
